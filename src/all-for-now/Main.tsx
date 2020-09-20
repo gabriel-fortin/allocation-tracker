cdd89cabe50969c3e33188710afdff35b3d990cd
gabi <gabi@pop-os.localdomain>
2020-09-20 21:02:03 +0100

    Define main structure

--- src/all-for-now/Main.tsx
+++ src/all-for-now/Main.tsx
@@ -1,15 +1,54 @@
 import React from "react";
-import { CSSReset, Button, ThemeProvider, theme } from "@chakra-ui/core";
+import { CSSReset, ThemeProvider, Flex, Divider, Text, BoxProps, Grid } from "@chakra-ui/core";
 
 export const Main: React.FC = () => {
     return (
         <ThemeProvider>
             <CSSReset />
-            Hello!
-
-            <Button variantColor="green">
-                test button for verifying css reset
-            </Button>
+            <Content />
         </ThemeProvider>
+    );
+};
+
+const Content: React.FC = () => (
+    <Flex
+        flexDirection="column"
+    >
+        <Header
+            padding={6}
+        />
+        <Divider
+            margin={0}
+        />
+        <Body
+            padding={6}
+        />
+    </Flex>
+);
+
+const Header: React.FC<BoxProps> = (boxProps) => {
+
+    return (
+        <Flex
+            {...boxProps}
+        >
+            <Text
+                // padding={2}
+            >
+                people
+            </Text>
+
+        </Flex>
+    );
+};
+
+const Body: React.FC<BoxProps> = (boxProps) => {
+
+    return (
+        <Grid
+            {...boxProps}
+        >
+            body
+        </Grid>
     );
 };
