e606e53c91bfd67342945e4583fddf47589d541d
gabi <gabi@pop-os.localdomain>
2020-10-03 14:38:26 +0100

    Remove initializer, add simple, inlined persistence

--- src/AppStore/Context.tsx
+++ src/AppStore/Context.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useContext } from "react";
+import React, { useState, useMemo, useContext, useEffect } from "react";
 
 import { Person, Project, Record } from "Model";
 import { AppStore, dummyAppStore } from "./AppStore";
@@ -9,32 +9,61 @@ const AppStoreContext = React.createContext<AppStore>(dummyAppStore);
 export const useAppStore: () => AppStore =
     () => useContext(AppStoreContext);
 
-export const AppStoreProvider: React.FC<{
-    initializer?: Promise<{ persons: Person[], projects: Project[], records: Record[] }>,
-}> = ({ initializer, children }) => {
+export const AppStoreProvider: React.FC = ({ children }) => {
     const [persons, setPersons] = useState<Person[]>([]);
     const [projects, setProjects] = useState<Project[]>([]);
     const [records, setRecords] = useState<Record[]>([]);
-    const [isLoading, setIsLoading] = useState(initializer !== undefined)
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    if (initializer) {
-        initializer.then(({ persons, projects, records }) => {
-            setIsLoading(false);
-            setPersons(persons);
-            setProjects(projects);
-            setRecords(records);
-        })
-    }
+    // TODO: persister should be a dependency, injected through props
+    const persister: {
+        retrievePersons: () => Person[],
+        // TODO: retrieveProjects: () => Project[],
+        // TODO: retrieveRecords: () => Record[],
+        storePersons: (persons: Person[]) => void,
+        // TODO: storeProjects
+        // TODO: storeRecords
+    } = {
+        retrievePersons: () => {
+            const retrievedPersons = window.localStorage.getItem("persons");
+            if (retrievedPersons === null) return [];
+            return JSON.parse(retrievedPersons);
+        },
+        storePersons: (persons) => {
+            window.localStorage.setItem("persons", JSON.stringify(persons));
+        },
+    };
+
+    useEffect(() => {
+        setPersons(persister.retrievePersons());
+
+        // TODO: load projects and records
+
+    }, []);
 
     const appStore: AppStore = useMemo(() => ({
         persons,
         projects,
         records,
-        addPerson: (newPerson) => setPersons(currentPersons => [newPerson, ...currentPersons]),
-        addProject: (newProject) => setProjects(currentProjects => [newProject, ...currentProjects]),
-        addRecord: (newRecord) => setRecords(currentRecords => [newRecord, ...currentRecords]),
         isLoading,
-    }), [persons, projects, records]);
+        addPerson: (newPerson) => {
+            const newPersonsCollection = [newPerson, ...persons];
+            setPersons(newPersonsCollection)
+            persister.storePersons(newPersonsCollection);
+        },
+        addProject: (newProject) => {
+            setProjects(currentProjects => [newProject, ...currentProjects]);
+
+            // TODO: persist projects
+
+        },
+        addRecord: (newRecord) => {
+            setRecords(currentRecords => [newRecord, ...currentRecords]);
+
+            // TODO: persist records
+
+        },
+    }), [persons, projects, records, isLoading]);
 
     return (
         <AppStoreContext.Provider value={appStore}>
