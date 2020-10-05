776d1afab9bb7e978d1234f6f512aa6ed8fa04ae
gabi <gabi@pop-os.localdomain>
2020-10-05 22:42:46 +0100

    Add skeleton code for Record Adder's modal

--- src/Component/RecordAdder/RecordAdder.tsx
+++ src/Component/RecordAdder/RecordAdder.tsx
@@ -1,15 +1,52 @@
-import { Button } from "@chakra-ui/core";
 import React from "react";
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/core";
 
 
 export const RecordAdder: React.FC = () => {
+    const { isOpen, onOpen: openModal, onClose: closeModal } = useDisclosure();
+
+    const userAcceptsNewRecord = () => {};
+
     return (
-        <Button
-            leftIcon="add"
-            variantColor="pink"
-            variant="solid"
-        >
-            Add record
-        </Button>
+        <>
+            <Button
+                leftIcon="add"
+                variantColor="pink"
+                variant="solid"
+                onClick={openModal}
+            >
+                Add record
+            </Button>
+
+            <Modal
+                onClose={closeModal}
+                isOpen={isOpen}
+                // initialFocusRef=the first field of the form, probably
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        Add record
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <NewRecordForm />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            variantColor="pink"
+                            variant="outline"
+                            onClick={userAcceptsNewRecord}
+                        >
+                            Add
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     );
+};
+
+const NewRecordForm: React.FC = () => {
+    return <>The form will go in here</>;
 };
