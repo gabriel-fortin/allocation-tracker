7e9c321c5904ff09d207253ab28c5556c5e9e938
gabi <gabi@pop-os.localdomain>
2020-10-11 19:26:50 +0100

    Use real projects in Data Table

--- src/Component/DataTable/useDataTableData.ts
+++ src/Component/DataTable/useDataTableData.ts
@@ -22,25 +22,25 @@ export const useDataTableData: () => Projects<Days<Persons<Value>>> = () => {
 
     // TODO: transform data from theAppStore into the required output structure
 
+    // TODO: replace Value with Record
+
     // for now, just some mock data
     return ({
-        projects: new Array(9)
-            .fill('X')
-            .map((_x, i) => ({
-                iid: i,
-                name: `project ${i}`,
-                days: new Array(3)
+        projects: theAppStore.projects
+            .map((project) => ({
+                ...project,
+                days: new Array(4)
                     .fill('?')
                     .map((_y, j) => ({
                         date: new Date(),
                         persons: [
-                            { iid: i*1000+j, firstName: `Gabriel`, initial: "G", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Shane`, initial: "S", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Kosta`, initial: "K", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Neill`, initial: "N", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Brett`, initial: "B", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Lee`, initial: "L", value: Math.random()>0.9 ? 1 : 0 },
-                            { iid: i*1000+j, firstName: `Jon`, initial: "J", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Gabriel`, initial: "G", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Shane`, initial: "S", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Kosta`, initial: "K", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Neill`, initial: "N", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Brett`, initial: "B", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Lee`, initial: "L", value: Math.random()>0.9 ? 1 : 0 },
+                            { iid: project.iid*1000+j, firstName: `Jon`, initial: "J", value: Math.random()>0.9 ? 1 : 0 },
                         ],
                     }))
             }))
