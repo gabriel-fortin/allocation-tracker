0260de72c88534574e6119423fd29b8468442773
gabi <gabi@pop-os.localdomain>
2020-10-13 21:24:07 +0100

    Finish implementing record adding form

--- src/Component/RecordAdder/RecordAdder.tsx
+++ src/Component/RecordAdder/RecordAdder.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Select } from "@chakra-ui/core";
+import { FormControl, FormLabel, Input, Select, Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@chakra-ui/core";
 
 import { ButtonWithLinkedModal, ModalButtonAction } from "Component";
 
@@ -7,6 +7,7 @@ import { useNewRecordFormData } from "./useNewRecordFormData";
 
 
 export const RecordAdder: React.FC = () => {
+    const data = useNewRecordFormData();
     const userAcceptsNewRecord: ModalButtonAction =
         (closeModal) => {
             console.log(`Record Adding  --  accepted`);
@@ -21,6 +22,7 @@ export const RecordAdder: React.FC = () => {
             modalButtonContent="Add"
             modalButtonAction={userAcceptsNewRecord}
             propsForTriggerButton={{ leftIcon: "add" }}
+            propsForModal={{ closeOnOverlayClick: false }}
         >
             <NewRecordForm />
         </ButtonWithLinkedModal>
@@ -30,12 +32,48 @@ export const RecordAdder: React.FC = () => {
 const NewRecordForm: React.FC = () => {
     const data = useNewRecordFormData();
     return (
-        <>
-            <Select placeholder="select person">
+        <FormControl
+        >
+            <FormLabel htmlFor="personSelection">
+                Person?
+            </FormLabel>
+            <Select
+                id="personSelection"
+                placeholder="select person"
+            >
                 {data.persons.map(person => (
                     <option key={person.iid} value={person.iid}>{person.firstName}</option>
                 ))}
             </Select>
-        </>
+            <FormLabel
+                htmlFor="dateSelection"
+                marginTop={4}
+            >
+                When?
+            </FormLabel>
+            <Input
+                id="dateSelection"
+                type="date"
+                defaultValue={new Date().toISOString().substring(0, 10)}
+                // defaultValue="2020-10-11"
+            />
+            <FormLabel
+                htmlFor="amountSelection"
+                marginTop={4}
+            >
+                Full day?
+            </FormLabel>
+            <Slider
+                id="amountSelection"
+                min={0}
+                max={1}
+                step={0.01}
+                defaultValue={1}
+            >
+                <SliderTrack />
+                <SliderFilledTrack />
+                <SliderThumb />
+            </Slider>
+        </FormControl>
     );
 };
